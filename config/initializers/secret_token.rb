f119d2d1d4d63a1ca84cbeb0f5177c04319155b8
Dev Bootcamp <Apprentice@dbc34.local>
2013-10-09 10:37:53 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brunoverflow::Application.config.secret_token = '5c9173abe673a607e4d0f00f2924a65fffec951df6ffe6fc7b1b8da03e4d8a02df29632415e2b80f58ece117306d444765967daf789642caccb555873ea5c246'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Brunoverflow::Application.config.session_store :cookie_store, key: '_brunoverflow_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Brunoverflow::Application.config.session_store :active_record_store
