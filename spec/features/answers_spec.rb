1684e1b348bc053ac6a7c17e9a0c9e7516f06b23
tomay <tom.allnutt@gmail.com>
2013-10-09 16:35:48 -0700

    oh god the merge is coming

--- spec/features/answers_spec.rb
+++ spec/features/answers_spec.rb
@@ -1,2 +1,26 @@
 require 'spec_helper'
 
+feature "Create answer" do
+  let(:new_answer) {FactoryGirl.build(:answer)}
+
+  before do
+    visit question_path(FactoryGirl.build(:question))
+  end
+
+  # after Answer.destroy_all?
+
+  # scenario "submitting the form without body does not add an answer to the question" do
+  # expect {
+  #   fill_in 'Body', with: nil
+  # }.to change(Answer, :count).by(1)
+
+  scenario "submitting the form adds an answer to the question" do
+    expect {
+      # new_answer
+      fill_in 'Body', with: new_answer.body
+    }.to change(Answer, :count).by(1)
+
+  end
+
+
+end
