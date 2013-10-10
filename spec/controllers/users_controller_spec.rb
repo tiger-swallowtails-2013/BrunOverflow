b568cbb91564af7b06e6fa73a9f622c7e244afe4
brunops <brunopsanches@gmail.com>
2013-10-09 19:34:30 -0700

    User model specs

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe UsersController do
+  describe 'POST #create' do
+    it "Should create a new user" do
+      expect {
+        post :create, {
+          user: {
+            name: 'madMax',
+            password: 'apples'
+          }
+        }
+      }.to change { User.count }.by 1
+    end
+  end
+end
