325ca1601708380b5e4c5411b53ad43902b2a88f
Dev Bootcamp <Apprentice@dbc35.local>
2013-10-10 12:25:43 -0700

    sessions controller WIP

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe SessionsController do
+  let(:valid_attributes) {{id: 2}}
+  it "creates a new session at login" do
+    # get :new
+    # expect(assigns(:user))
+  end
+
+
+end
