d20c26e652896bb2569b09ba6e93b2bf7fc68835
tomay <tom.allnutt@gmail.com>
2013-10-11 12:35:58 -0700

    added email to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131010035928) do
+ActiveRecord::Schema.define(:version => 20131011193421) do
 
   create_table "answers", :force => true do |t|
     t.text     "body"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20131010035928) do
     t.string   "password"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "email"
   end
 
 end
