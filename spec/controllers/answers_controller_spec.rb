786012b319bc3b60a41a7e1b5f9ea11c68f0fcd1
Bruno Sanches <brunopsanches@gmail.com>
2013-10-11 20:55:53 -0700

    Fix indenting

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -9,17 +9,17 @@ describe AnswersController do
   end
 
   describe "POST answer#create" do
-    let (:new_answer) {FactoryGirl.build(:answer)}
+    let (:new_answer) { FactoryGirl.build(:answer) }
 
     it "should create a new answer" do
-      post :create, answer: {body: new_answer.body, question_id: new_answer.question_id}
+      post :create, answer: { body: new_answer.body, question_id: new_answer.question_id }
       expect(assigns(:answer)).to be_a(Answer)
       expect(assigns(:answer).body).to eq new_answer.body
       expect(assigns(:answer).question_id).to eq new_answer.question_id
     end
 
     it "should create an answer associated with a question" do
-      post :create, answer: {body: new_answer.body, question_id: new_answer.question_id}
+      post :create, answer: { body: new_answer.body, question_id: new_answer.question_id }
       expect(assigns(:answer).question).to be_a(Question)
     end
   end
